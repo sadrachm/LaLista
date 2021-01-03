10c55dec70ec43ebab362bc9da8fe66a82b63cf0
Sadrach Martinez <63802470+sadrachm@users.noreply.github.com>
2021-01-03 11:50:26 -0800

    Cleaned workspace
    Removed personal info and removed extraneous files

--- src/Project1/RunEmail.java
+++ src/Project1/RunEmail.java
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class attemt{
+public class RunEmail{
 	public static void main(String[] args) throws IOException {
 		Process p = Runtime.getRuntime().exec("python C:/Users/sadra/ICS_45J/LaLista/src/Project1/emailAttempt.py");
 	}
--- src/Project1/SendEmail.py
+++ src/Project1/SendEmail.py
@@ -1,24 +1,21 @@
 import smtplib, ssl
-#print("HOLA")
-#f = open(FinalList)
+
 f = open("C:/Users/sadra/ICS_45J/LaLista/FinalList.txt", "r")
 x = f.read()
 
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
-sender_email = "turkeysandwich0@gmail.com"
-receiver_email = "turkeysandwich0@gmail.com"
-password = "Sadrach2"
+sender_email = input("Gmail: ")
+password = input("Password: ")
+receiver_email = input("Receiving Email: ")
 message = """\
-Subject: Hi there
+Subject: Lista de Mercado!
 
 """
 message += x
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
     server.starttls(context=context)
-    server.ehlo()  # Can be omitted
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
